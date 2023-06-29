cc5b6906007208f1aa371cf5edf3defd50fa550e
Hmic1102 <113384319+Hmic1102@users.noreply.github.com>
2023-06-29 16:21:26 -0400

    Update oxford_test.py

--- finetune/oxford_test.py
+++ finetune/oxford_test.py
@@ -223,7 +223,7 @@ def main_worker(gpu, ngpus_per_node, args):
             args.start_epoch = 0
             best_acc1 = 0
             state_dict = checkpoint['state_dict']
-            print(state_dict['fc.bias'].shape)
+            print(state_dict['module.fc.bias'].shape)
             print(model.state_dict()['module.fc.bias'].shape)
             state_dict['fc.bias'] = model.state_dict()['module.fc.bias']
             state_dict['fc.weight'] = model.state_dict()['module.fc.weight']
