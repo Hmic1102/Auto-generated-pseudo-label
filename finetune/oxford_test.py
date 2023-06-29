0528794ba9bc07d85e85ade636471312950189de
Hmic1102 <113384319+Hmic1102@users.noreply.github.com>
2023-06-29 16:22:22 -0400

    Update oxford_test.py

--- finetune/oxford_test.py
+++ finetune/oxford_test.py
@@ -225,8 +225,8 @@ def main_worker(gpu, ngpus_per_node, args):
             state_dict = checkpoint['state_dict']
             print(state_dict['module.fc.bias'].shape)
             print(model.state_dict()['module.fc.bias'].shape)
-            state_dict['fc.bias'] = model.state_dict()['module.fc.bias']
-            state_dict['fc.weight'] = model.state_dict()['module.fc.weight']
+            state_dict['module.fc.bias'] = model.state_dict()['module.fc.bias']
+            state_dict['module.fc.weight'] = model.state_dict()['module.fc.weight']
             model.load_state_dict(state_dict)
             print("=> loaded checkpoint '{}' ".format(args.resume))
             
