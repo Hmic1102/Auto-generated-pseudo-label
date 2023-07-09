589c19a8567749079690767c0244d43917bf71b6
Hmic1102 <113384319+Hmic1102@users.noreply.github.com>
2023-07-09 14:00:28 -0400

    Create ucf101_test.py

---
+++ finetune/ucf101_test.py
@@ -0,0 +1,325 @@
+import os
+import numpy as np
+import cv2
+import time
+import h5py
+
+def getUCF101(base_directory = ''):
+
+    # action class labels
+    class_file = open(base_directory + 'ucfTrainTestlist/classInd.txt','r')
+    lines = class_file.readlines()
+    lines = [line.split(' ')[1].strip() for line in lines]
+    class_file.close()
+    class_list = np.asarray(lines)
+
+    # training data
+    train_file = open(base_directory + 'ucfTrainTestlist/trainlist01.txt','r')
+    lines = train_file.readlines()
+    filenames = ['UCF-101/' + line.split(' ')[0] for line in lines]
+    y_train = [int(line.split(' ')[1].strip())-1 for line in lines]
+    y_train = np.asarray(y_train)
+    filenames = [base_directory + filename for filename in filenames]
+    train_file.close()
+
+    train = (np.asarray(filenames),y_train)
+
+    # testing data
+    test_file = open(base_directory + 'ucfTrainTestlist/testlist01.txt','r')
+    lines = test_file.readlines()
+    filenames = ['UCF-101/' + line.split(' ')[0].strip() for line in lines]
+    classnames = [filename.split('/')[1] for filename in filenames]
+    y_test = [np.where(classname == class_list)[0][0] for classname in classnames]
+    y_test = np.asarray(y_test)
+    filenames = [base_directory + filename for filename in filenames]
+    test_file.close()
+
+    test = (np.asarray(filenames),y_test)
+
+    return class_list, train, test
+
+
+def loadFrame(args):
+    mean = np.asarray([0.485, 0.456, 0.406],np.float32)
+    std = np.asarray([0.229, 0.224, 0.225],np.float32)
+
+    curr_w = 320
+    curr_h = 240
+    height = width = 224
+    (filename,augment) = args
+
+    data = np.zeros((3,height,width),dtype=np.float32)
+
+    try:
+        ### load file from HDF5
+        filename = filename.replace('.avi','.hdf5')
+        filename = filename.replace('UCF-101','UCF-101-hdf5')
+        h = h5py.File(filename,'r')
+        nFrames = len(h['video']) - 1
+        frame_index = np.random.randint(nFrames)
+        frame = h['video'][frame_index]
+
+        if(augment==True):
+            ## RANDOM CROP - crop 70-100% of original size
+            ## don't maintain aspect ratio
+            if(np.random.randint(2)==0):
+                resize_factor_w = 0.3*np.random.rand()+0.7
+                resize_factor_h = 0.3*np.random.rand()+0.7
+                w1 = int(curr_w*resize_factor_w)
+                h1 = int(curr_h*resize_factor_h)
+                w = np.random.randint(curr_w-w1)
+                h = np.random.randint(curr_h-h1)
+                frame = frame[h:(h+h1),w:(w+w1)]
+            
+            ## FLIP
+            if(np.random.randint(2)==0):
+                frame = cv2.flip(frame,1)
+
+            frame = cv2.resize(frame,(width,height))
+            frame = frame.astype(np.float32)
+
+            ## Brightness +/- 15
+            brightness = 30
+            random_add = np.random.randint(brightness+1) - brightness/2.0
+            frame += random_add
+            frame[frame>255] = 255.0
+            frame[frame<0] = 0.0
+
+        else:
+            # don't augment
+            frame = cv2.resize(frame,(width,height))
+            frame = frame.astype(np.float32)
+
+        ## resnet model was trained on images with mean subtracted
+        frame = frame/255.0
+        frame = (frame - mean)/std
+        frame = frame.transpose(2,0,1)
+        data[:,:,:] = frame
+    except:
+        print("Exception: " + filename)
+        data = np.array([])
+    return data
+
+
+import numpy as np
+import os
+import sys
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+#from torchvision import datasets, transforms
+from torch.autograd import Variable
+import torch.distributed as dist
+import torchvision
+
+from helperFunctions import getUCF101
+from helperFunctions import loadFrame
+
+import h5py
+import cv2
+
+from multiprocessing import Pool
+
+IMAGE_SIZE = 224
+NUM_CLASSES = 101
+batch_size = 100
+lr = args.lr
+num_of_epochs = args.epochs
+
+
+data_directory = '/projects/training/bayw/hdf5/UCF-101-hdf5/'
+class_list, train, test = getUCF101(base_directory = data_directory)
+
+
+model =  torchvision.models.resnet50(pretrained=True)
+model.fc = nn.Linear(2048,NUM_CLASSES)
+
+model.cuda()
+
+optimizer = optim.Adam(params,lr=lr)
+criterion = nn.CrossEntropyLoss()
+
+pool_threads = Pool(8,maxtasksperchild=200)
+
+
+
+for epoch in range(0,num_of_epochs):
+
+    ###### TRAIN
+    train_accu = []
+    model.train()
+    random_indices = np.random.permutation(len(train[0]))
+    start_time = time.time()
+    for i in range(0, len(train[0])-batch_size,batch_size):
+
+        augment = True
+        video_list = [(train[0][k],augment)
+                       for k in random_indices[i:(batch_size+i)]]
+        data = pool_threads.map(loadFrame,video_list)
+
+        next_batch = 0
+        for video in data:
+            if video.size==0: # there was an exception, skip this
+                next_batch = 1
+        if(next_batch==1):
+            continue
+
+        x = np.asarray(data,dtype=np.float32)
+        x = Variable(torch.FloatTensor(x)).cuda().contiguous()
+
+        y = train[1][random_indices[i:(batch_size+i)]]
+        y = torch.from_numpy(y).cuda()
+
+        output = model(x)
+
+        loss = criterion(output, y)
+        optimizer.zero_grad()
+
+        loss.backward()
+        optimizer.step()
+        
+        prediction = output.data.max(1)[1]
+        accuracy = ( float( prediction.eq(y.data).sum() ) /float(batch_size))*100.0
+        if(epoch==0):
+            print(i,accuracy)
+        train_accu.append(accuracy)
+    accuracy_epoch = np.mean(train_accu)
+    print(epoch, accuracy_epoch,time.time()-start_time)
+
+torch.save(model,'single_frame.model')
+pool_threads.close()
+pool_threads.terminate()
+
+
+
+
+##### TEST
+model.eval()
+test_accu = []
+random_indices = np.random.permutation(len(test[0]))
+t1 = time.time()
+for i in range(0,len(test[0])-batch_size,batch_size):
+    augment = False
+    video_list = [(test[0][k],augment) 
+                    for k in random_indices[i:(batch_size+i)]]
+    data = pool_threads.map(loadFrame,video_list)
+
+    next_batch = 0
+    for video in data:
+        if video.size==0: # there was an exception, skip this batch
+            next_batch = 1
+    if(next_batch==1):
+        continue
+
+    x = np.asarray(data,dtype=np.float32)
+    x = Variable(torch.FloatTensor(x)).cuda().contiguous()
+
+    y = test[1][random_indices[i:(batch_size+i)]]
+    y = torch.from_numpy(y).cuda()
+
+    output = model(x)
+
+    prediction = output.data.max(1)[1]
+    accuracy = ( float( prediction.eq(y.data).sum() ) /float(batch_size))*100.0
+    test_accu.append(accuracy)
+    accuracy_test = np.mean(test_accu)
+print('Testing',accuracy_test,time.time()-t1)
+
+
+
+model = torch.load('single_frame.model')
+model.cuda()
+
+##### save predictions directory
+prediction_directory = 'UCF-101-predictions/'
+if not os.path.exists(prediction_directory):
+    os.makedirs(prediction_directory)
+for label in class_list:
+    if not os.path.exists(prediction_directory+label+'/'):
+        os.makedirs(prediction_directory+label+'/')
+
+
+
+
+acc_top1 = 0.0
+acc_top5 = 0.0
+acc_top10 = 0.0
+confusion_matrix = np.zeros((NUM_CLASSES,NUM_CLASSES),dtype=np.float32)
+random_indices = np.random.permutation(len(test[0]))
+mean = np.asarray([0.485, 0.456, 0.406],np.float32)
+std = np.asarray([0.229, 0.224, 0.225],np.float32)
+model.eval()
+
+
+
+
+for i in range(len(test[0])):
+
+    t1 = time.time()
+
+    index = random_indices[i]
+
+    filename = test[0][index]
+    filename = filename.replace('.avi','.hdf5')
+    filename = filename.replace('UCF-101','UCF-101-hdf5')
+
+    h = h5py.File(filename,'r')
+    nFrames = len(h['video'])
+
+    data = np.zeros((nFrames,3,IMAGE_SIZE,IMAGE_SIZE),dtype=np.float32)
+
+    for j in range(nFrames):
+        frame = h['video'][j]
+        frame = frame.astype(np.float32)
+        frame = cv2.resize(frame,(IMAGE_SIZE,IMAGE_SIZE))
+        frame = frame/255.0
+        frame = (frame - mean)/std
+        frame = frame.transpose(2,0,1)
+        data[j,:,:,:] = frame
+    h.close()
+
+
+
+
+prediction = np.zeros((nFrames,NUM_CLASSES),dtype=np.float32)
+
+loop_i = list(range(0,nFrames,200))
+loop_i.append(nFrames)
+
+for j in range(len(loop_i)-1):
+  data_batch = data[loop_i[j]:loop_i[j+1]]
+
+  with torch.no_grad():
+    x = np.asarray(data_batch,dtype=np.float32)
+    x = Variable(torch.FloatTensor(x)).cuda().contiguous()
+    output = model(x)
+  
+  prediction[loop_i[j]:loop_i[j+1]] = output.cpu().numpy()
+
+
+filename = filename.replace(data_directory+'UCF-101-hdf5/',prediction_directory)
+if(not os.path.isfile(filename)):
+  with h5py.File(filename,'w') as h:
+    h.create_dataset('predictions',data=prediction)
+
+# softmax
+    
+for j in range(prediction.shape[0]):
+  prediction[j] = np.exp(prediction[j])/np.sum(np.exp(prediction[j]))
+
+prediction = np.sum(np.log(prediction),axis=0)
+argsort_pred = np.argsort(-prediction)[0:10]
+
+label = test[1][index]
+confusion_matrix[label,argsort_pred[0]] += 1
+if(label==argsort_pred[0]):
+  acc_top1 += 1.0
+if(np.any(argsort_pred[0:5]==label)):
+  acc_top5 += 1.0
+if(np.any(argsort_pred[:]==label)):
+  acc_top10 += 1.0
+
+print('i:%d nFrames:%d t:%f (%f,%f,%f)' % (i,nFrames,time.time()-t1,acc_top1/(i+1),acc_top5/(i+1), acc_top10/(i+1)))
