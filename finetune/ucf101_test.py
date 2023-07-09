6cf99164fe58f981300d8c70bf34e32a8a230de9
Hmic1102 <113384319+Hmic1102@users.noreply.github.com>
2023-07-09 15:00:12 -0400

    Update ucf101_test.py

--- finetune/ucf101_test.py
+++ finetune/ucf101_test.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import cv2
 import time
-import h5py
+
 
 
 
@@ -135,7 +135,8 @@ import torch.distributed as dist
 import torchvision
 
 
-import h5py
+
+
 import cv2
 
 from multiprocessing import Pool
